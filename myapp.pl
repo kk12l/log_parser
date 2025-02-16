69ce9d6d41e9178ca85dd6b0c54d34a2a41e1141
kk12l <mlmtree@gmail.com>
2025-02-16 21:49:27 +0200

    Рефакторинг для соответствия контексту.

--- myapp.pl
+++ myapp.pl
@@ -11,17 +11,15 @@ plugin Database => {
 	dbi_attr => { 'AutoCommit' => 1, 'RaiseError' => 1, 'PrintError' =>1 },
 	on_connect => sub {
 		my $dbh = shift or die $DBI::errstr;
-		#$dbh->do("DELETE FROM message");
-		#$dbh->do("DELETE FROM log");
 	}
 };
 
 my %flags = (
-	'<=' => 2, #Отбор в таблицу message
-	'=<' => 1,
+	'<=' => 4, # Указаны веса для сортировки по хронологии
+	'**' => 3,
+	'==' => 3,
+	'=>' => 2,
 	'->' => 1,
-	'**' => 1,
-	'==' => 1,
 );
 
 sub send_db {
@@ -48,73 +46,97 @@ sub check_datetime {
 get '/load' => sub ($c) {
 	my ( $message_count, $log_count, $total_count ) = ( 0, 0, 0 );
 
+	# Очистка данных от предыдущей загрузки
+	$c->db->do("DELETE FROM message");
+	$c->db->do("DELETE FROM log");
+
 	open my $fh, '<', 'out';
 	while (my $row = <$fh>) {
 		chomp $row;
 		$total_count++;
-		my @portions = split ' ', $row;
 
-		my %values;
-		if ( $portions[0] =~ /^\d\d\d\d-\d\d-\d\d$/ && $portions[1] =~ /^\d\d:\d\d:\d\d$/ ) {
-			$values{created} = join ' ', $portions[0], $portions[1];
-		}
+		my ( %values, $possible_flag, $rest_row );
+		# Первые поля с постоянным размером - выделяются по размеру полей
+		# Поле флага не всегда заполнено флагом - учитывается это
+		( $values{created}, $values{int_id}, $possible_flag, $rest_row ) = unpack 'A19xA16xA2A*', $row;
+
+		# Проверка, что БД понимает выделенное из строки время
 		unless ( check_datetime $c->db, $values{created} ) {
 			app->log->error( sprintf "Not valid date: %s", $row );
 			next;
 		} else {
-			$values{str} = join ' ', @portions[ 2 .. $#portions ];
+			$values{str} = join ' ', $values{int_id}, ( $possible_flag . $rest_row );
 		}
 
-		if ( $portions[2] =~ /^\w{6}-\w{6}-\w\w$/ ) {
-			$values{int_id} = $portions[2];
-		}
+		# Проверка на обязательное поле int_id
 		# 2012-02-13 15:00:55 SMTP connection from [109.70.26.4] (TCP/IP connection count = 1)
-		unless ( $values{int_id} ) {
+		unless ( $values{int_id} =~ /^\w{6}-\w{6}-\w\w$/ ) {
 			app->log->warn( sprintf "Not valid int_id: %s", $row );
 			next;
 		}
 
-		( undef, $values{id} ) = split 'id=', $portions[-1];
-		if ( $values{id} && $flags{ $portions[3] } && $flags{ $portions[3] } > 1 ) {
-			# 2012-02-13 14:39:22 1RwtJa-0009RI-KL <= tpxmuwr@somehost.ru H=mail.somehost.com [84.154.134.45] P=esmtp S=1716 id=120213143628.BLOCKED.453962@whois.somehost.ru
+		# Остальные поля выделяются по предположения, что поля разделены пробелом и всегда начинаются
+		# с буквы и знака равно. Например " H=".
+		my ( $possible_address, %data ) = split /\s(\w=)/, $rest_row;
 
+		# В поле S= есть id, который выделяется по ТЗ
+		( undef, $values{id} ) = split 'id=', $data{'S='} if $data{'S='};
+		if ( $values{id} && $flags{$possible_flag} && $flags{$possible_flag} == 4 ) {
+			# 2012-02-13 14:39:22 1RwtJa-0009RI-KL <= tpxmuwr@somehost.ru H=mail.somehost.com [84.154.134.45] P=esmtp S=1716 id=120213143628.BLOCKED.453962@whois.somehost.ru
 			send_db( $c->db, 'message', %values );
+
 			$message_count++;
 		} else {
-			# Есть флаг
-			if ( $flags{ $portions[3] } ) {
+			# Указан флаг
+			if ( $flags{$possible_flag} ) {
 				# 2012-02-13 15:00:55 1RwteR-000Om4-65 == psqgg@yandex.ru R=dnslookup T=remote_smtp defer (-1): domain matches queue_smtp_domains, or -odqs set
-				$values{address} = $portions[4];
+				# " psqgg@yandex.ru" - убирается лидирующий пробел
+				( $values{address} ) = unpack 'xA*', $possible_address;
 			}
 
 			send_db( $c->db, 'log', %values );
+
 			$log_count++;
 		}
 	}
 	close $fh;
 
-	$c->db->commit;
 	$c->render(template => 'load', messages => $message_count, logs => $log_count, total => $total_count );
 };
 
 get '/search' => sub ($c) {
 	my $query = $c->param('query') || '';
 
+	# Выборка int_id с указанным получателем с лимитом больше 100 для вывода на форме, что данных больше
 	my @int_ids = $c->db->selectall_array(
 		"SELECT DISTINCT int_id FROM log WHERE address LIKE ? ORDER BY int_id, created LIMIT 101",
 		undef,
 		sprintf( '%%%s%%', $query ),
 	);
+	# Выборка записей с найденными int_id с лимитом 150 что бы уменьшить разорванность хронологии,
+	# которая ниже собирается по флагам (время слишком дискретно для этого)
 	my $sql = sprintf(
 		"
 			SELECT created, str, int_id FROM log WHERE int_id IN (%s)
 			UNION ALL SELECT created, str, int_id FROM message WHERE int_id IN (%s)
-			ORDER BY int_id, created LIMIT 101
+			ORDER BY int_id LIMIT 150
 		",
 		( "'" . join( "','", map { $_->[0] } @int_ids ) . "'" ) x 2,
 	);
 	my @rows = $c->db->selectall_array($sql);
 	my $is_exceeded = @rows > 100 ? 1 : 0;
+
+	# Если в одну секунду проходит вся цепочка обработки почты, то сортировка по флагу для удобного
+	# восприятия хронологии - поступление, попытки отправки, отправка, завершающее сообщение.
+	# Так как структура БД описана в ТЗ и не предусматривает колонку флага, то собирается "на лету".
+	# Добавляется возможный флаг к выборке
+	push @$_, unpack 'x17A2', $_->[1] for @rows;
+	@rows = sort {
+		$a->[2] cmp $b->[2]                                               # int_id
+		|| $a->[0] cmp $b->[0]                                            # created
+		|| ( $flags{ $b->[3] } || 0 ) <=> ( $flags{ $a->[3] } || 0 )      # flag
+	} @rows;
+
 	@rows = @rows[ 0 .. 99 ];
 	$c->render( template => 'search', rows => \@rows, query => $query, is_exceeded => $is_exceeded );
 };
